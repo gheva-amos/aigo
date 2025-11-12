aa6abd21484db4674f5b8c58e84b7822062f92de
Amos Gheva <ghevaamos@gmail.com>
2025-11-11 21:51:04 -0500

    Saving, in an unsecure way, to an image file

---
+++ mazes/imager.go
@@ -0,0 +1,51 @@
+package maze
+
+import (
+	"image"
+	"image/color"
+	"image/draw"
+	"image/png"
+	"os"
+)
+
+const cell_size = 60
+
+var (
+	empty   = color.RGBA{B: 255, A: 255}
+	in_path = color.RGBA{G: 150, A: 150}
+	wall    = color.RGBA{A: 255}
+	start   = color.RGBA{G: 255, A: 255}
+	end     = color.RGBA{R: 255, A: 255}
+)
+
+func (m *Maze) ToImage(solution *Solution, path string) {
+	canvas := image.NewRGBA(image.Rect(0, 0, m.Height()*cell_size, m.Width()*cell_size))
+	draw.Draw(canvas, canvas.Bounds(), &image.Uniform{color.NRGBA{30, 30, 30, 255}}, image.Point{}, draw.Src)
+	for row, row_elements := range m.Board {
+		for col, cell := range row_elements {
+			color := empty
+			if solution.Contains(cell.Coords) {
+				color = in_path
+			}
+			if cell.IsWall {
+				color = wall
+			}
+			if cell.Coords.Equals(m.Start) {
+				color = start
+			}
+			if cell.Coords.Equals(m.End) {
+				color = end
+			}
+			m.draw_rect(row, col, cell_size, canvas, color)
+		}
+	}
+	f, _ := os.Create(path)
+	defer f.Close()
+	png.Encode(f, canvas)
+}
+
+func (m *Maze) draw_rect(x, y, size int, canvas *image.RGBA, color color.RGBA) {
+	cell := image.NewRGBA(image.Rect(0, 0, size, size))
+	draw.Draw(cell, cell.Bounds(), &image.Uniform{color}, image.Point{}, draw.Src)
+	draw.Draw(canvas, image.Rect(x*size, y*size, (x+1)*size, (y+1)*size), cell, image.Point{}, draw.Src)
+}
