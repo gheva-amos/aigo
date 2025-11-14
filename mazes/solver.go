db6f2b3c78ff9f11f34813d05fcf7953690fa936
Amos Gheva <ghevaamos@gmail.com>
2025-11-14 16:58:11 -0500

    A utility method for the solver class

--- mazes/solver.go
+++ mazes/solver.go
@@ -1,9 +1,5 @@
 package maze
 
-import (
-	"fmt"
-)
-
 type PathFollower interface {
 	Step(*Maze, Point)
 	Done(*Maze, *Solution)
@@ -29,10 +25,13 @@ func (s *Solver) End() Point {
 	return s.maze.End
 }
 
+func (s *Solver) At(row, col int) *Node {
+	return &s.maze.Board[row][col]
+}
+
 func (s *Solver) Step(point Point) {
 	s.road_taken = append(s.road_taken, point)
 	s.did_visit[point] = true
-	fmt.Printf("(%d, %d)\n", point.Row, point.Col)
 	for _, f := range s.followers {
 		f.Step(s.maze, point)
 	}
